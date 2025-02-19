8e329059633f23fc0ab21e767562df94774d65a9
Arthur Asasira <asasiraarthur@gmail.com>
2025-02-19 21:40:04 +0200

    chatbot

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,2 +1,10 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
+
+
+class ChatbotView(APIView):
+    def post(self, request):
+        return Response({'message': 'Hello, world!'},status=status.HTTP_200_OK)
